1c4edbdfab8bca8ab64a49f55d052b96dd68e7fa
Nejca13 <nicolascontreras677@gmail.com>
2022-08-30 10:20:13 -0300

    navbar mobile

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react"
 import styles from "./NavBar.module.css"
 import Link from "next/link"
 import Logo from "../Logo/Logo"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faBars, faXmark } from "@fortawesome/free-solid-svg-icons"
 
 function NavBar() {
+  const [hambStatus, setHambStatus] = useState(faBars)
+  
+  const hamb = () => {
+    let menu = document.getElementById('hambMenu')
+    menu.classList.toggle(styles.active)
+    if(hambStatus == faBars){
+      setHambStatus(faXmark)
+    }
+    if(hambStatus == faXmark){
+      setHambStatus(faBars)
+    }
+    document.getElementById('ulMenu').classList.toggle(styles.active)
+    
+  }
   return (
     <>
       <div className={styles.container}>
-        {/* <Image src={logo} alt="Logo de la Empresa." className={styles.Logo} width={80} height={80} /> */}
         <Logo/>
-        <ul>
-          <li><Link href="#_1">Inicio</Link></li>
-          <li><Link href="#about">Nosotros</Link></li>
-          <li><Link href="#service">Servicios</Link></li>
-          <li><Link href="#">Contacto</Link></li>
+        
+          <FontAwesomeIcon icon={hambStatus} id="hambMenu" className={styles.hambMenu} onClick={hamb}/>
+        
+        <ul className={styles.mobileNav} id="ulMenu">
+          <li onClick={hamb}><Link href="#_1">Inicio</Link></li>
+          <li onClick={hamb}><Link href="#about">Nosotros</Link></li>
+          <li onClick={hamb}><Link href="#service">Servicios</Link></li>
+          <li onClick={hamb}><Link href="#contact">Contacto</Link></li>
         </ul>
       </div>
     </>
